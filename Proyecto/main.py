bbdd0902f669bc1217358deaec35d80657b427b5
ChamiloHM08 <camilo.hernandez2020@alu.uct.cl>
2021-09-17 00:41:23 -0300

    Update main.py
    Agregue ruta "Crear documento"

--- Proyecto/main.py
+++ Proyecto/main.py
@@ -10,10 +10,13 @@ def hello_world():
 def sube_archivo():
     return "<p>Aqui se sube el archivo</p>"
 
-
 @app.route("/editararquetipos")
-def editararquetipos():
+def edita_rarquetipos():
     return "<p>Aqui se editar los arquetipos</p>"
+
+@app.route("/creardocumento")
+def crear_documento():
+    return "<p>Aqui se crean los documentos</p>"
 
 if __name__ == "__main__":
     app.run()
