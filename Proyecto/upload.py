0a7bb8abb120433590950b2f30007cb558b17ef6
B3nja2020 <bmeier2020@alu.uct.cl>
2021-09-24 00:00:12 -0300

    Create upload.py
    subida de archivo

---
+++ Proyecto/upload.py
@@ -0,0 +1,21 @@
+import os
+from flask import Flask, render_template, request
+from werkzeug import secure_filename
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = './ArchivosJSON'
+
+@app.route("/")
+def upload_file():
+  return render_template('main.html')
+
+@app.route("/subirarchivo", methods=['POST'])
+def uploader():
+  if request.method == 'POST':
+    f = request.files['archivo']
+    filename = secure_filename(f.filename)
+    f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    return "<h1>Archivo subido exitosamente</h1>"
+
+if __name__ == '__main__':
+ app.run(debug=True)
