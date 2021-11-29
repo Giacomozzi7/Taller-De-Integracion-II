cd039a2e32b747fdbb9181309b89ea069588cedb
B3nja2020 <bmeier2020@alu.uct.cl>
2021-11-29 00:46:10 -0300

    Update editor1.js

--- Proyecto/frontend/static/javascript/editor1.js
+++ Proyecto/frontend/static/javascript/editor1.js
@@ -1,5 +1,9 @@
 var editor1 = new RichTextEditor("#div_editor1");
 
 function downloadPDFDialog() {
-    editor1.execCommand("html2pdf");
+  editor1.execCommand("html2pdf");
+}
+
+function exportaData(){
+  document.getElementById("hola").innerHTML = editor1.getPlainText();
 }
